7010dfdf047824a0c6a67fb59655b77e759b6819
Thomas <Thomas@Thomas>
2016-11-30 17:31:51 -0500

    Login Servlet

---
+++ UBook/src/java/controller/SearchServlet.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Thomas
+ */
+@WebServlet(name = "SearchServlet", urlPatterns = {"/searchISBN"})
+public class SearchServlet extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (request.getParameter("submit") != null) {
+            HttpSession session = request.getSession(true);
+            session.setAttribute("searchisbn", request.getParameter("isbn"));
+            forwardRequest(request, response, "/results.jsp");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /*
+     * Forward this request to another component. 
+     */
+    private void forwardRequest(HttpServletRequest request,
+            HttpServletResponse response, String forwardUrl)
+            throws IOException, ServletException {
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(
+                forwardUrl);
+        dispatcher.forward(request, response);
+    }
+}
